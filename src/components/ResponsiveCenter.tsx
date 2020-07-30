02215a31f0e29bac0e4f0f3d261f9821a1277bc7
Jack Sun <syy1125@hotmail.com>
2020-07-30 16:08:17 -0700

    refactor: Use auto layout for spacers

--- src/components/ResponsiveCenter.tsx
+++ src/components/ResponsiveCenter.tsx
@@ -9,15 +9,11 @@ interface Props {
 export const ResponsiveCenter = ({ children }: Props) => {
   return (
     <Grid container>
-      <Hidden smDown>
-        <Grid item md={1} lg={2} xl={3} />
-      </Hidden>
+      <Grid item xs />
       <Grid item xs={12} md={10} lg={8} xl={6}>
         {children}
       </Grid>
-      <Hidden smDown>
-        <Grid item md={1} lg={2} xl={3} />
-      </Hidden>
+      <Grid item xs />
     </Grid>
   )
 }
